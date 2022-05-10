6426b88b1ef2b0dc0f83d23057b98281874a58d1
Sean Long <sean.n.long@outlook.com>
2022-05-10 12:51:21 +0800

    • Replaced the default unhandled error dialog in Windows Forms with Message Box

--- DG Engineering/Program.cs
+++ DG Engineering/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DG_Engineering
@@ -16,7 +13,13 @@ namespace DG_Engineering
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += Application_ThreadException;
             Application.Run(new LoginWindow());
+        }
+
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
         }
     }
 }
