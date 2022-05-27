f0f4fd71c23bcabe37f61c4de7d60f09256e11d2
Sean Long <sean.n.long@outlook.com>
2022-05-27 15:02:40 +0800

    • Version 1.2.013 • Added Multiple Scheduled Task options

--- DG Engineering/Main Window.cs
+++ DG Engineering/Main Window.cs
@@ -18,7 +18,7 @@ namespace DG_Engineering
         #region Main Window Load
         private async void MainWindow_Load(object sender, EventArgs e)
         {
-            VersionLabel.Text = @"Version 1.2.012";
+            VersionLabel.Text = @"Version 1.2.013";
             var environment = await CoreWebView2Environment.CreateAsync(null, Path.GetTempPath());
             await JobsTabViewer.EnsureCoreWebView2Async(environment);
             await AdminJobViewer.EnsureCoreWebView2Async(environment);
